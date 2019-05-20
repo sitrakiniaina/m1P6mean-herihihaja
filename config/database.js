9593a24b83f4461700efdb04b99ca1f4972cfcfe
herinihaja <rasendrahasinarivo.herinihaja@gmail.com>
2019-05-20 23:15:32 +0300

    loading

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
   module.exports = {
-     
-      database : "mongodb+srv://herinihaja:herinihaja@cluster0-u9apk.mongodb.net/test?retryWrites=true",
+      //database: "mongodb://localhost:27017/myproject",      
+      database: "mongodb://herinihaja:herinihaja@cluster0-shard-00-00-u9apk.mongodb.net:27017,cluster0-shard-00-01-u9apk.mongodb.net:27017,cluster0-shard-00-02-u9apk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
       secret:"herinihaja"
   }
